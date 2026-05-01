ae491187767e65a95d80ecb4db0d365cb719e10b
GeovaneSilvaP <geovanesil45sil@gmail.com>
2026-05-01 10:01:21 -0300

    at

--- back_end/src/db/connection.ts
+++ back_end/src/db/connection.ts
@@ -2,6 +2,7 @@ import mysql, { Connection } from "mysql2";
 
 export const connection: Connection = mysql.createConnection({
   host: process.env.DB_HOST as string,
+  port: Number(process.env.DB_PORT),
   user: process.env.DB_USER as string,
   password: process.env.DB_PASSWORD as string,
   database: process.env.DB_NAME as string,
